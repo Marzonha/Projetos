b4630e10bef48f452f611b5c25830792a9ec1302
Marlon <marzonhapacheco@gmail.com>
2025-11-08 17:27:52 -0300

    Foi colocado o caminho para o perfil e também o fundo do site e a fonte usada no mesmo

--- ljroupas/src/App.jsx
+++ ljroupas/src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css'
-
+// App.jsx - Configuração das rotas
+import { Routes, Route } from "react-router-dom";
+// Importação do css
+import "./style.global.css";
+// Importção da Header
 import Header from "./Pages/Header";
 
+import Perfil from "./Pages/Perfil";
 function App() {
-  
   return (
     <>
-     <Header/>
+        <Header />
+        <Routes>
+          <Route path="/perfil" element={<Perfil />}></Route>
+        </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
