043bcae1f9653833938f75b758e79990833e3c2e
Marlon <marzonhapacheco@gmail.com>
2025-11-08 19:54:48 -0300

    Ajustado para conter os caminhos entre a Home e perfil de usuário na header, ouve um erro em relação ao conteúdo/texto de teste da página do perfil o texto ficou por cima do icone de avatar e o texto "meu perfil "

--- ljroupas/src/Pages/Header.jsx
+++ ljroupas/src/Pages/Header.jsx
@@ -1,19 +1,33 @@
-// Header.jsx - VERSÃO CORRIGIDA
-import { FiSearch } from "react-icons/fi";
+// Dependências
+import { Link, Routes, Route } from "react-router-dom";
+
+// Componentes
 import Navbar from "../components/Navbar";
 import Usuario from "../components/Usuario";
+
+// Pages
+import Perfil from "./Perfil";
+
+// CSS
 import style from "../CSS/header.module.css";
 
 export default function Header() {
   return (
     <header className={style.Header}>
+      <Link to="/">
+        <img src="" alt="Logo da Loja" />
+      </Link>
+
       {/* Navbar centralizada */}
       <div className={style.Rltv}>
         <Navbar />
       </div>
-      
+
       {/* Usuário no canto direito */}
       <div className={style.usuarioContainer}>
+        <Routes>
+          <Route path="/perfil" element={<Perfil />}></Route>
+        </Routes>
         <Usuario />
       </div>
     </header>
